04cc2138704041e400c44b10dd0d7b2a02533aa4
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-03-07 11:08:44 +0900

    [Application03 push]

--- chap02-statements-lecture-sourcesd/src/main/java/com/ohgiraffers/section01/statement/Application03.java
+++ chap02-statements-lecture-sourcesd/src/main/java/com/ohgiraffers/section01/statement/Application03.java
@@ -4,24 +4,14 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
-public class Application02 {
+public class Application03 {
 
     public static void main(String[] args) {
-
-        /* 필기.
-        *   순서
-        *   1. Connection 생성
-        *   2. Statement 생성
-        *   3. ResultSet 생성
-        *   4. Connection 의 createStatement() 메소드를 이용해서 Statement 인스턴스 생성
-        *   5. executeQuery() 로 쿼리문을 실행하고 결과를 ResultSet 으로 반환받음
-        *   6. ResultSet 에 담긴 결과물을 컬럼 이름(label) 을 이용해 꺼내어 출력
-        *   7. 마지막으로 사용한 자원 반납 (Connection, Statement, ResultSet)
-        *  */
 
         // 1. Connection 생성
         Connection con = getConnection();
@@ -32,13 +22,16 @@ public class Application02 {
         // 3. ResultSet 생성
         ResultSet rset = null;
 
-
-
         try {
             // 4. Connection 의 createStatement() 메소드를 이용해서 Statement 인스턴스 생성
             stmt = con.createStatement();
 
-            String empId = "202";
+            // Scanner 사용해서 응용
+            Scanner sc = new Scanner(System.in);
+            System.out.print("조회하실 직원의 사번을 입력해주세요 : ");
+            String empId = sc.nextLine();
+
+
             String query = "SELECT EMP_ID, EMP_NAME FROM EMPLOYEE WHERE EMP_ID = '" + empId + "'";
 
             // 5. executeQuery() 로 쿼리문을 실행하고 결과를 ResultSet 으로 반환받음
