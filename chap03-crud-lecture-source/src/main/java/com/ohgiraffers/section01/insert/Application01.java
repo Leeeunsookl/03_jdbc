fac860697de34056cc18bf449384719908276a01
pyunghun <raccoon.ohgiraffers@gmail.com>
2024-03-07 17:02:08 +0900

    [수업]

--- chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section01/insert/Application01.java
+++ chap03-crud-lecture-source/src/main/java/com/ohgiraffers/section01/insert/Application01.java
@@ -4,8 +4,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Properties;
 
+import static com.ohgiraffers.common.JDBCTemplate.close;
 import static com.ohgiraffers.common.JDBCTemplate.getConnection;
 
 public class Application01 {
@@ -26,10 +28,26 @@ public class Application01 {
             String query = prop.getProperty("insertMenu");
 
             System.out.println("query = " + query);
-            
+
+            pstmt = con.prepareStatement(query);
+
+            pstmt.setString(1, "봉골레청국장");
+            pstmt.setInt(2, 50000);
+            pstmt.setInt(3, 1);
+            pstmt.setString(4, "Y");
+
+            result = pstmt.executeUpdate();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(pstmt);
+            close(con);
         }
+
+        System.out.println("result = " + result);
 
     }
 
